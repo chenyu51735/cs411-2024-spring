d57b9bf7e595f335a401b541bb91cffbd72879ea
Yu Chen <kane@bu.edu>
2024-04-12 19:46:11 -0400

    fixed typo

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', method=['GET', 'POST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     data = request.form
     print(data)
@@ -13,11 +13,12 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/sign-up', method=['GET', 'POST'])
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         firstname = request.form.get('firstname')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+
     return render_template("sign_up.html")
