7f0f9cb7ba6564aa6579d2da8cc48b6bc861ec36
Yu Chen <144164106+chenyu51735@users.noreply.github.com>
2024-04-12 21:15:27 -0400

    Add files via upload

--- website/auth.py
+++ website/auth.py
@@ -2,10 +2,8 @@ from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/login')
 def login():
-    data = request.form
-    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -13,12 +11,16 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route('/sign-up')
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         firstname = request.form.get('firstname')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-
     return render_template("sign_up.html")
+
+
+@auth.route('/home')
+def home():
+    return render_template("home.html")
